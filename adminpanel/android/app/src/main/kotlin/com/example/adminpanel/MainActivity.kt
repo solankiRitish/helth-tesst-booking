c1aeec69d4a1bae4de86401ee09189360bd64994
solankiRitish <140726419+solankiRitish@users.noreply.github.com>
2024-10-17 08:27:08 +0530

    remove

--- adminpanel/android/app/src/main/kotlin/com/example/adminpanel/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.adminpanel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- helthtestbookingapp/android/app/src/main/kotlin/com/example/helthtestbookingapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.helthtestbookingapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
